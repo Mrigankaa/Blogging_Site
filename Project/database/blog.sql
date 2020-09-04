47bbaad800a227a7cf5893fa28e097e22761889b
Mriganka Choudhury <49262873+Mrigankaa@users.noreply.github.com>
2020-09-04 09:31:55 +0530

    Update blog.sql

--- Project/database/blog.sql
+++ Project/database/blog.sql
@@ -1,8 +1,14 @@
-
 CREATE TABLE users (
-    id int(11) NOT NULL PRIMARY KEY AUTO INCREMENT,
-    name VARCHAR(255) NOT NULL,
+    id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
 );
 
+create TABLE blogs (
+  blog_id int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  blog Varchar(255) NOT NULL,
+  user_id INT(11),
+  FOREIGN KEY(user_id) REFERENCES users(id)
+  )
